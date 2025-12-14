5035da80ad5836423f87363d8684f1f43930743d
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2025-12-14 15:42:43 +0300

    Room Class #2: added their respective getters & setters

--- src/Room.java
+++ src/Room.java
@@ -10,4 +10,39 @@ public class Room {
         this.isOccupied = isOccupied;
     }
 
+    //add getters
+    public int getRoomNum() {
+        return roomNum;
+    }
+
+    public double getRoomPrice() {
+        return roomPrice;
+    }
+
+    public double getVAT() {
+        return VAT;
+    }
+
+    public boolean IsOccupied() {
+        return isOccupied;
+    }
+
+    
+    //add setters
+    public void setRoomNum(int roomNum) {
+        this.roomNum = roomNum;
+    }
+
+    public void setRoomPrice(double roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    public void setVAT(double VAT) {
+        this.VAT = VAT;
+    }
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
 }
