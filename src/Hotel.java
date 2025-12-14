c574bcecfd588fc309e4a10d728ef71cf4b48532
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2025-12-14 18:45:50 +0300

    Hotel Class #3:added their getters & setters

--- src/Hotel.java
+++ src/Hotel.java
@@ -5,6 +5,7 @@ public class Hotel {
    private int totalRooms;
    private int totalReservation;
 
+
    public Hotel(String hotelName, String hotelLocation, int buildingStories,int totalRooms,int totalReservations){
        this.hotelName=hotelName;
        this.hotelLocation=hotelLocation;
@@ -12,6 +13,48 @@ public class Hotel {
        this.totalRooms=totalRooms;
        this.totalReservation=totalReservations;
    }
-  
 
+
+   // Add getters
+   public String getHotelName() {
+        return hotelName;
+    }
+    public String getHotelLocation(){
+       return hotelLocation;
+
+    }
+
+    public int getBuildingStories() {
+        return buildingStories;
+    }
+
+    public int getTotalRooms() {
+        return totalRooms;
+    }
+
+    public int getTotalReservation() {
+        return totalReservation;
+    }
+
+
+   //add Setters
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public void setHotelLocation(String hotelLocation) {
+        this.hotelLocation = hotelLocation;
+    }
+
+    public void setBuildingStories(int buildingStories) {
+        this.buildingStories = buildingStories;
+    }
+
+    public void setTotalRooms(int totalRooms) {
+        this.totalRooms = totalRooms;
+    }
+
+    public void setTotalReservation(int totalReservation) {
+        this.totalReservation = totalReservation;
+    }
 }
