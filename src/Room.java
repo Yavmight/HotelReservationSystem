52956b132cb2a6166e850ae2191bcc2d59071537
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2025-12-14 06:22:32 +0300

    Room Class #2:added the attributes and their constructor

--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,13 @@
 public class Room {
+    private int roomNum;
+    private double roomPrice;
+    private double VAT;
+    private boolean isOccupied;
+
+     public Room(int roomNum, double roomPrice, boolean isOccupied) {
+        this.roomNum = roomNum;
+        this.roomPrice = roomPrice;
+        this.isOccupied = isOccupied;
+    }
+
 }
