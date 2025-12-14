fee4f1f4d84f563861b1a631e3e6a588db5d71ab
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2025-12-15 00:51:35 +0300

    Customer Class #4:added the attributes and their constructor

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,15 @@
 public class Customer {
+    private String Fname;
+    private String Lname;
+    private int age;
+    private String  email;
+    private int phone;
+
+    public Customer(String Fname,String Lname,int age,String email, int phone){
+        this.Fname=Fname;
+        this.Lname=Fname;
+        this.age=age;
+        this.email=email;
+        this.phone=phone;
+    }
 }
