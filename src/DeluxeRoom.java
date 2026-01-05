96b10722b20d12dc83a9581d9b8ad66b3508a2f3
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2026-01-06 02:27:12 +0300

    #12 Reworked the Deluxeroom Child class to match with the Room Parent Class

--- src/DeluxeRoom.java
+++ src/DeluxeRoom.java
@@ -1,23 +1,30 @@
 public class DeluxeRoom extends Room {
 
-    private double DeluxeFee;
+    private double basePrice;
+    private double deluxeFee;
 
-    public DeluxeRoom(int roomNum, double roomPrice, boolean isOccupied, double serviceFee) {
-        super(roomNum,isOccupied);
-        this.DeluxeFee = serviceFee;
+    public DeluxeRoom(int roomNum, double basePrice, double deluxeFee) {
+        super(roomNum);
+        this.basePrice = basePrice;
+        this.deluxeFee = deluxeFee;
     }
 
     public double getDeluxeFee() {
-        return DeluxeFee;
+        return deluxeFee;
     }
 
-    public void setDeluxeFee(double DeluxeFee ) {
-        this.DeluxeFee = DeluxeFee;
+    public void setDeluxeFee(double deluxeFee) {
+        this.deluxeFee = deluxeFee;
     }
 
-
+    @Override
     public double calculatePrice(int nights, double vatRate) {
-        double subtotal = (getRoomPrice() + getDeluxeFee()) * nights;
+        double subtotal = (basePrice + deluxeFee) * nights;
         return subtotal + (subtotal * vatRate);
+    }
+
+    @Override
+    public String getRoomType() {
+        return "Deluxe";
     }
 }
