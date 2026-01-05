15b2993457a517682a4732903ef1e5717215231f
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2026-01-05 08:57:19 +0300

    #8 Payment Class: Added the attributes & Constructor

--- src/Payment.java
+++ src/Payment.java
@@ -1,2 +1,36 @@
+import java.time.LocalDateTime;
+
 public class Payment {
+
+    public enum PaymentMethod { CASH, CARD }
+
+    private PaymentMethod paymentMethod;
+    private double amount;
+    private boolean isPaid;
+    private LocalDateTime paidAt;
+
+
+    public Payment(PaymentMethod paymentMethod, double amount) {
+        this.paymentMethod = paymentMethod;
+        this.amount = amount;
+        this.isPaid = false;
+        this.paidAt = null;
+    }
+
+
+    public PaymentMethod getMethod() {
+        return paymentMethod;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public LocalDateTime getPaidAt() {
+        return paidAt;
+    }
 }
