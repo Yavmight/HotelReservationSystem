acb829fe8ab5fb1bdaafa6da907b76a3e23b3865
Abdulrahman Alkhalidi <abdulrahmankhalidi003@gmail.com>
2026-01-05 07:03:43 +0300

    #7 StandardRoom:Added the standardRoom child class with its constructor and  getters & setters

---
+++ src/StandardRoom.java
@@ -0,0 +1,11 @@
+public class StandardRoom extends Room {
+    private double baseRoomPrice;
+
+    public StandardRoom(int roomNum, double roomPrice, boolean isOccupied, double serviceFee, double baseRoomPrice) {
+        super(roomNum, roomPrice, isOccupied);
+        this.baseRoomPrice = baseRoomPrice;
+    }
+
+    public double getBaseRoomPrice() { return baseRoomPrice; }
+    public void setBaseRoomPrice(double baseRoomPrice) { this.baseRoomPrice = baseRoomPrice; }
+}
